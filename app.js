2e3782fa05189702809ca99138601dc1d1fbd883
Koyel Nath <60928116+koyelnath98@users.noreply.github.com>
2020-05-31 10:46:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,27 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   
     apiCallFromRequest.callApi(function(response){
-
+               //showing all data in console
+                 console.log(response);
              res.render('show',{'datas': response } );
                
             });   
 });
+
+app.get('/trade/:id', (req, res) => {
+      // find the trade 
+      apiCallFromRequest.callApi(function(response){
+      const trade = response.filter((trade) => {
+        return trade.id == req.params.id
+      })[0]
+      // render the `trade.ejs` template with the trade content
+      res.render('trade', {
+        'data': trade
+      });
+
+    });
+
+    })
 
 app.listen(3000, function () {
   console.log(' yahoo Express app listening on port 3000');
